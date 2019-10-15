addd5cd4d0b29ae9fbe59aff54f139f5a86f9201
Mr programmer <55302148+mrprogrammer-69@users.noreply.github.com>
2019-10-15 07:22:18 +0530

    Add files via upload

--- daycounter.py
+++ daycounter.py
@@ -9,15 +9,15 @@ current_day = int(time.strftime("%d"))
 current_month = int(time.strftime("%m"))
 current_year = int(time.strftime("%y"))+2000
 
+while True:
+    user_raw_input = input("Please Enter Your Date Of Birth In \n>>>Format DD/MM/YYYY\n : ")
+    split = user_raw_input.split("/")
 
-user_raw_input = input("Please Enter Your Date Of Birth In \n In Format DD/MM/YYYY ")
-split = user_raw_input.split("/")
+    dobdate = int(split[0])
+    dobmonth = int(split[1])
+    dobyear = int(split[2])
 
-dobdate = int(split[0])
-dobmonth = int(split[1])
-dobyear = int(split[2])
-      
 
-date1 = date(dobyear, dobmonth, dobdate) 
-date2 = date(current_year,current_month,current_day)
-print("Its Been ",numOfDays(date1, date2), "days Since You Were Born") 
+    date1 = date(dobyear, dobmonth, dobdate) 
+    date2 = date(current_year,current_month,current_day)
+    print("Its Been ",numOfDays(date1, date2), "days Since You Were Born \n  ", "OR" ,(numOfDays(date1, date2))*24 , "Hours\n")  
